de3672de167443ec31e02446eb3902568686bb8c
alexandra.d <alexandra.oduron@gmail.com>
2020-04-01 09:29:40 +0100

    Added connection to DB, and retrieve data from it.

--- UberApp/src/UberApp/DatabaseConnection.java
+++ UberApp/src/UberApp/DatabaseConnection.java
@@ -1,3 +1,5 @@
+/*Connection to the DB */
+
 package UberApp;
 
 import java.sql.DriverManager;
@@ -6,38 +8,34 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class DatabaseConnection {
+public class DatabaseConnection{
+
+	//Global Attributes
+	private Connection connection = null;
+	private Statement statement = null;
+	private ResultSet result = null;
 
 
-	public static void main(String[] argv) {
+	/** Load the JDBC driver.*/
+	public void connectDriver() {
 
-		//Load the JDBC driver.
 		try {
 			Class.forName("org.postgresql.Driver");
-			System.out.println("PostgreSQL JDBC Driver found!");
+			//System.out.println("PostgreSQL JDBC Driver found!");
 		} catch (ClassNotFoundException e) {
 			System.out.println("Could not find JDBC Driver");
 			e.printStackTrace();
 		} 
-
-		//Connect to the database.
-		Connection connection = null;
-		Statement statement = null;
-		ResultSet result = null;
-		try {
-			connection = DriverManager.getConnection("jdbc:postgresql://ec2-46-137-177-160.eu-west-1.compute.amazonaws.com:5432/d8voi6728iiep5", "wauipwcqgqausi","889da4fe285c07ac13694c4ce1ffdadca243aec76adf2c5b75481128b9ec2a87");
-
-			try {
-				//Retrieve users 
-				statement = connection.createStatement();
-				result = statement.executeQuery("SELECT * FROM drivers;"); 
-				while (result.next()) {
-					  System.out.println(result.getString("user_name") + " " +  result.getString("location"));
-					}
+	}
 	
-			}catch(Exception e){ System.out.println("User not found");}
+	
 
-		} catch (SQLException e) {
+	/** Connect to the database.*/
+	public void connectDatabase(){
+
+		try {
+			connection = DriverManager.getConnection("jdbc:postgresql://ec2-46-137-177-160.eu-west-1.compute.amazonaws.com:5432/d8voi6728iiep5", "wauipwcqgqausi","889da4fe285c07ac13694c4ce1ffdadca243aec76adf2c5b75481128b9ec2a87");	
+		}catch (SQLException e) {
 			System.out.println("Connection Failed!");
 			e.printStackTrace();
 		}  finally {
@@ -48,8 +46,38 @@ public class DatabaseConnection {
 					e.printStackTrace();
 				}
 			}
-		}
+		}	
 	}
+	
+
+	
+
+	/** Retrieve Drivers*/
+	public void retrieveDrivers() {
+
+		try {
+			statement = connection.createStatement();
+			result = statement.executeQuery("SELECT * FROM drivers;"); 
+			while (result.next()){
+				System.out.println("\nDiver's user name: "+result.getString("user_name")+ "     Location: "+ result.getString("location"));
+			}
+		}catch(Exception e){ System.out.println("Drivers not found");}
+	}
+	
+	
+
+	/** Retrieve Passengers*/
+	public void retrievePassengers() {
+
+		try {
+			statement = connection.createStatement();
+			result = statement.executeQuery("SELECT * FROM passengers;"); 
+			while (result.next()) {
+				System.out.println("\nPassenger's user name: "+result.getString("user_name")+ "     Location: "+ result.getString("location"));
+			}
+		}catch(Exception e){ System.out.println("Passengers not found");}	
+	}
+
 } 
 
 
--- UberApp/src/UberApp/Main.java
+++ UberApp/src/UberApp/Main.java
@@ -17,6 +17,19 @@ public class Main{
 
 
 	public static void main(String[] args){
+		
+		
+		
+		//Test database. We will change this. 
+		DatabaseConnection connect = new DatabaseConnection();
+		connect.connectDriver();
+		connect.connectDatabase();
+		connect.retrieveDrivers();
+		connect.retrievePassengers();
+
+		
+		
+		
 
 		Scanner scanner = new Scanner(System.in); 
 
