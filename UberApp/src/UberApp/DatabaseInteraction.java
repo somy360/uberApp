69f6895a6b7ab3fd87ee39e91482928cb4983000
alexandra.d <alexandra.oduron@gmail.com>
2020-04-03 00:11:33 +0100

    Added methods to interact with the database. Changed menu.

---
+++ UberApp/src/UberApp/DatabaseInteraction.java
@@ -0,0 +1,107 @@
+/*Interaction with the DB.*/
+
+package UberApp;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseInteraction{
+
+	//Global Attributes
+	private Connection connection = null;
+	private Statement statement = null;
+	private ResultSet result = null;
+
+	/** Connect to the database.*/
+	public void connectDatabase(){
+
+		try {
+			connection = DriverManager.getConnection("jdbc:postgresql://ec2-46-137-177-160.eu-west-1.compute.amazonaws.com:5432/d8voi6728iiep5", "wauipwcqgqausi","889da4fe285c07ac13694c4ce1ffdadca243aec76adf2c5b75481128b9ec2a87");
+		}catch (Exception e) {
+			System.out.println("Connection Failed!");
+			e.printStackTrace();
+		}  finally {
+			if (statement != null) { 
+				try {
+					statement.close();
+				}catch (Exception e){
+					e.printStackTrace();
+				}
+			}
+		}	
+	}
+
+
+	/** Retrieve Drivers*/
+	public void retrieveDrivers(){
+
+		try {
+			statement = connection.createStatement();
+			result = statement.executeQuery("SELECT * FROM drivers;"); 
+			while (result.next()){
+				System.out.println("\nDiver's user name: "+result.getString("user_name")+ "     Location: "+ result.getString("location"));
+			}
+		}catch(Exception e){ System.err.println("Drivers not found");}
+	}
+
+
+	/** Retrieve Passengers*/
+	public void retrievePassengers(){
+
+		try {
+			statement = connection.createStatement();
+			result = statement.executeQuery("SELECT * FROM passengers;"); 
+			while (result.next()) {
+				System.out.println("\nPassenger's user name: "+result.getString("user_name")+ "     Location: "+ result.getString("location"));
+			}
+		}catch(Exception e){ System.err.println("Passengers not found");}	
+	}
+
+
+	/** Insert a passenger in the table*/	
+	public void insertPassenger(String userName, String passengerLocation){		
+		try {	
+			statement = connection.createStatement();
+			statement.executeUpdate("INSERT INTO passengers " + "VALUES ('" + userName + "', '" + passengerLocation +"')");		
+		} catch (SQLException e) {
+			System.err.println("Sorry! Passenger not inserted in database.");
+			e.printStackTrace();
+		}
+	}
+
+	/** Insert a driver in the table*/
+	public void insertDriver(String userName, String driverLocation){		
+		try {
+			statement = connection.createStatement();
+			statement.executeUpdate("INSERT INTO drivers " + "VALUES ('" + userName + "', '" + driverLocation +"')");		
+		} catch (SQLException e) {
+			System.err.println("Sorry! Driver not inserted in database.");
+			e.printStackTrace();
+		}
+	}
+
+
+	/**Check for a valid non repeated passenger's user name, or for a valid non repeated diver's user name. * */
+	public boolean checkNonRepeatedUserName(String userName){
+
+		boolean repeatedUserName = false;
+		String SQL = "SELECT * FROM passengers WHERE user_name='" + userName + "'";
+		try {
+			statement = connection.createStatement();
+			result  = statement.executeQuery(SQL);	
+			if(result.next()){
+				repeatedUserName = true;
+			}
+		}catch (SQLException e){
+			System.err.println("Sorry! We could not check for a valid user name.");
+			e.printStackTrace();
+		}
+		return repeatedUserName;	
+	}
+
+
+
+
+} 
--- UberApp/src/UberApp/Main.java
+++ UberApp/src/UberApp/Main.java
@@ -3,122 +3,116 @@
  * Building a software product that enables people to arrange transport for a fee with independent drivers
    looking to make money by providing a taxi service.  
 
-   @author  Graeme Somerville, Alexandra Durón
+   @author  Alexandra Durón, Graeme Somerville.
 
  */
 
 package UberApp;
-
-import java.util.ArrayList;
+import java.sql.Statement;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class Main{
 
-
 	public static void main(String[] args){
-		
-		
-		
-		//Test database. We will change this. 
-		DatabaseConnection connect = new DatabaseConnection();
-		connect.connectDriver();
+
+		//Connect to database.
+		DatabaseInteraction connect = new DatabaseInteraction();
 		connect.connectDatabase();
-		connect.retrieveDrivers();
-		connect.retrievePassengers();
 
-		
-		
-		
+		//These two methods are just for testing, we will delete them. 
+		//connect.retrieveDrivers();
+		//connect.retrievePassengers();
 
-		Scanner scanner = new Scanner(System.in); 
-
-		//Get username from the user
-		System.out.print("\nPlease enter your name: ");
-		String userName = scanner.nextLine(); 
-
-		//Get user type from the user
-		System.out.println("\nHello! "+ userName +". \nPlease enter P to access as a passenger, or type D to access as a driver:");
-		char option = scanner.nextLine().charAt(0);
-
-		//Select passenger or driver.
-		//while(true) {
-		if(option=='P'||option=='p'){	
-
-			System.out.println("Passenger");
-
-			//get passengers location from user
-			System.out.print("In which city are you located?: ");
-			String location = scanner.nextLine();
-
-			//get passengers destination from user
-			System.out.print("\nPlease enter your destination:");
-			String destination = scanner.nextLine();
-
-			//create the passenger
-			Passenger passenger = new Passenger(userName, location);
-
-			//display the list of drivers for the passengers city
-			System.out.println("Driver List: ");
-			List<Driver> drivers = passenger.getDrivers(location); //get the list of all drivers for a specific location
-			for(int i = 0; i < drivers.size(); i++) { //loop for all drivers in the driver list
-				System.out.println(drivers.get(i).getUserName()); //print each drivers username
+		// The user name identifies the user uniquely. Check for a valid user name.
+		System.out.print("\nWelcome!");
+		Scanner scanner = new Scanner(System.in);
+		String userName = null;
+		do{
+			System.out.print("\nPlease enter a user name: ");
+			userName = scanner.nextLine();
+			if(connect.checkNonRepeatedUserName(userName)){
+				System.out.println("Sorry, it looks like that username is already taken. Please try again.");
 			}
+		}while(connect.checkNonRepeatedUserName(userName));
+		System.out.println("\nHello "+ userName+"!" );
 
-			//select the driver and request the ride
-			//get the driver from user
-			System.out.println("\nPlease select a driver by entering their username: ");
-			String driverName = scanner.nextLine();
 
-			//fetch the correct driver from the list based on the username
-			Driver driver = null;
-			for(int i = 0; i < drivers.size(); i++) {
-				if(drivers.get(i).getUserName().contentEquals(driverName)) {
-					driver = drivers.get(i); //if the usernames match
+		////Menu.
+		while(true){
+
+			System.out.print("Please choose an option: "+ "\n\nEnter P to access as a passenger.\nEnter D to access as a driver.\nEnter E to exit the program.");
+			char option = scanner.nextLine().charAt(0);
+
+			//Get the passenger's location, and insert the passenger in the database.Get passenger's destination.
+			if(option=='P'||option=='p'){	
+
+				System.out.print("\nPassenger. \nIn which city are you located?: ");
+				String passengerLocation = scanner.nextLine();
+				try {
+					connect.insertPassenger(userName,passengerLocation);	
+				}catch(Exception e){
+					System.out.println("Insertion failed");
+					e.printStackTrace();}								
+				System.out.print("\nPlease enter your destination:");
+				String destination = scanner.nextLine();
+
+
+				//Graeme's code. Show a list of drivers and select one. 
+				//Create the passenger.
+				Passenger passenger = new Passenger(userName, passengerLocation);
+
+				//Display the list of drivers for the passengers city.
+				System.out.println("Driver List: ");
+				List<Driver> drivers = passenger.getDrivers(passengerLocation); //get the list of all drivers for a specific location
+				for(int i = 0; i < drivers.size(); i++) { //loop for all drivers in the driver list
+					System.out.println(drivers.get(i).getUserName()); //print each drivers username
 				}
-			}			
 
-			//create the ride
-			Ride ride = new Ride(passenger, driver, location, destination);
+				//Select the driver and request the ride.
+				//Get the driver from user.
+				System.out.println("\nPlease select a driver by entering their username: ");
+				String driverName = scanner.nextLine();
 
-			//request the ride
-			passenger.requestRide(ride);
+				//Fetch the correct driver from the list based on the user name.
+				Driver driver = null;
+				for(int i = 0; i < drivers.size(); i++) {
+					if(drivers.get(i).getUserName().contentEquals(driverName)) {
+						driver = drivers.get(i); //if the user names match.
+					}
+				}			
+				//Create the ride.
+				Ride ride = new Ride(passenger, driver, passengerLocation, destination);
+				
+				//Request the ride.
+				passenger.requestRide(ride);
+				System.out.println(driverName + "  will be with you shortly....");
 
-			System.out.println(driverName + "  will be with you shortly....");
+				//Get the driver's location, and insert the driver in the database.	
+			}else if(option=='D'||option=='d'){
+				
+				System.out.println("\nHello Driver! In which city are you located?");
+				String driverLocation = scanner.nextLine();	
+				try {
+					connect.insertDriver(userName,driverLocation);
+				}catch(Exception e){
+					System.out.println("Insertion failed");
+					e.printStackTrace();}
 
+				//Graeme's code.
+				//create the driver (and add to server - still to be implemented)
+				Driver driver = new Driver(userName, driverLocation);
+				System.out.println("Waiting for a passenger.....");	
+
+				//Option to exit the system.
+			}else if(option=='E'||option=='e'){
+				System.out.println("\nGood bye "+userName+"!  We hope to see you soon!");
+				break; 
+			}else {
+				System.out.println("\nThis is not a valid input. Please try again.\n");
+			}
 		}
-		else if(option=='D'||option=='d'){
-
-			System.out.println("Driver");
-
-			//get the Drivers location from user
-			System.out.println("\nIn which city are you located?");
-			String location = scanner.nextLine();
-
-			//create the driver (and add to server - still to be implemented)
-			Driver driver = new Driver(userName, location);
-			System.out.println("Waiting for a passenger.....");	
-
-		}
-
-		else {
-			System.out.println("\nThis is not a valid option. Please try again.");
-		}
-		//}
-
-		scanner.close(); //close the scanner after we have finished using it
-
+		scanner.close();
 	}
-
-
-	/*Spare code snipets can be used later
-	 * 
-	 * if(location.contentEquals("0")) {System.exit(0);}; //exit program if input is 0
-	 *
-	 *		//Give user the option to exit the program
-	 *	//			else if(option=='1'){
-	 *	//				System.out.println("\nGood bye "+userName);
-	 *	//				System.exit(0); 
-	 *	//}
-	 */
 }
